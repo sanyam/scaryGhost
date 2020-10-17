5413240740300e61763c6cd1eb8d2a241fb0ab74
Alexander Myagchilov <myagchilov@gmail.com>
2020-10-17 18:30:34 +0300

    WERWER

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("Игра - Страшный призрак!")
 clock = pygame.time.Clock()
 fps = 60
 # comment Dima 3
-# cooment 3
+# cooment 4
 # устанавлваем фон черный
 black = [0, 0, 0]
 x = 50
