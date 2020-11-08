a2d9a3af004ca70d6f609fc0e4dad4ac8b1b3902
Alexander Myagchilov <myagchilov@gmail.com>
2020-11-08 16:12:10 +0300

    Перевел все на классы, сделал загрузку уровня из текстового файла

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys, pygame
+import cProfile
+import re
 from wall import Wall
 from player import Player
 from ghost import Ghost
+from door import Door
+
+cProfile.run('re.compile("foo|bar")')
 
 # Инициализация игрового движка PyGame
 pygame.init()
@@ -10,26 +15,31 @@ FPS = 60
 clock = pygame.time.Clock()
 
 # устанавливаем размер окна
-screen = pygame.display.set_mode((1360, 700))
+screen = pygame.display.set_mode((1360, 760), pygame.HWSURFACE | pygame.DOUBLEBUF)
 # устанавливаем заголовок окна
 pygame.display.set_caption("Игра - Страшный призрак")
 
 # создаем группу спрайтов стен
 walls = pygame.sprite.Group()
+doors = pygame.sprite.Group()
 # загружаем данные из файла level0.txt, где описана структура уровня (0 - ничего нет, 1 - стена)
 level_lines = open("level0.txt","r").readlines()
 i = 0
 for line in level_lines:
     j = 0
-    for ch in line:
-        if ch == "1":
-            walls.add(Wall((j * 40, i * 36)))
+    for blockType in line:
+        if blockType == "1":
+            walls.add(Wall((j * 40, i * 40)))
+        elif blockType == "2":
+            doors.add(Door((j * 40, i * 40), True))
+        elif blockType == "3":
+            doors.add(Door((j * 40, i * 40), False))
         j = j + 1
     i = i + 1
 i = 0
 
 # создаем игрока
-player = Player((1260, 580));
+player = Player((1260, 600));
 
 #coздаем приведение
 ghost = Ghost((100, 100));
@@ -44,13 +54,16 @@ while True:
             sys.exit()
 
     #двигаем игрока
-    player.handle_move(event, walls)
+    player.handle_move(event, walls, doors)
 
     # заполняем поле черным
     screen.fill(pygame.Color(0, 0, 0))
     #рисуем стены
     for wall in walls:
         wall.draw(screen)
+    # рисуем двери
+    for wall in doors:
+        doors.draw(screen)
     # рисуем игрока
     player.draw(screen)
     #рисуем приведение
--- player.py
+++ player.py
@@ -20,6 +20,9 @@ class Player(pygame.sprite.Sprite):
         self.x_motion = Move.STOP
         self.y_motion = Move.STOP
 
+        self.door_nearby = None
+        self.space_is_down = False
+
     def draw(self, screen):
         # рисуем картинку игрока в зависимости от его текущего движения
         if self.y_motion == Move.DOWN or (self.x_motion == Move.STOP and self.y_motion == Move.STOP):
@@ -31,7 +34,7 @@ class Player(pygame.sprite.Sprite):
         elif self.x_motion == Move.RIGHT:
             screen.blit(self.player_images[3], (self.rect.x, self.rect.y))
 
-    def handle_move(self, event, walls):
+    def handle_move(self, event, walls, doors):
 
         # обрабатывем нажание кнопок и реагируем на них
         if event.type == pygame.KEYDOWN:
@@ -43,11 +46,21 @@ class Player(pygame.sprite.Sprite):
                 self.y_motion = Move.UP
             elif event.key == pygame.K_DOWN:
                 self.y_motion = Move.DOWN
+            elif event.key == pygame.K_SPACE:
+                if not self.space_is_down:
+                    if self.door_nearby is not None:
+                        self.door_nearby.change_state()
+                self.space_is_down = True
+
         elif event.type == pygame.KEYUP:
             if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
                 self.x_motion = Move.STOP
             if event.key in [pygame.K_UP, pygame.K_DOWN]:
                 self.y_motion = Move.STOP
+            if event.key == pygame.K_SPACE:
+                self.space_is_down = False
+
+
         #print('Current move is: ' + self.x_motion.value + ' ' + self.y_motion.value)
 
         prev_x = self.rect.x
@@ -55,18 +68,36 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 3
         elif self.x_motion == Move.RIGHT:
             self.rect.x += 3
+
+        # проверяем столкновение со стеной слева или справа
         collide = pygame.sprite.spritecollide(self, walls, False, False)
         if collide:
             self.rect.x = prev_x
+
+        # проверяем столкновение со дверью слева или справа
+        collide = pygame.sprite.spritecollide(self, doors, False, False)
+        for door in collide:
+            if not door.is_opened:
+                self.rect.x = prev_x
+            self.door_nearby = door
 
         prev_y = self.rect.y
         if self.y_motion == Move.UP:
             self.rect.y -= 3
         elif self.y_motion == Move.DOWN:
             self.rect.y += 3
+
+        # проверяем столкновение со стеной сверху или снизу
         collide = pygame.sprite.spritecollide(self, walls, False, False)
         if collide:
             self.rect.y = prev_y
+
+        # проверяем столкновение со дверью сверху или снизу
+        collide = pygame.sprite.spritecollide(self, doors, False, False)
+        for door in collide:
+            if not door.is_opened:
+                self.rect.y = prev_y
+            self.door_nearby = door
 
         #print('Current position is: {} {}',self.rect.x, self.rect.y)
 
